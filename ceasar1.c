cebe693ad35123fd1e85f3cfd6d48f4489ea4084
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-02-01 05:48:23 +0800

    Update ceasar1.c

--- ceasar1.c
+++ ceasar1.c
@@ -3,41 +3,57 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-
+// **
+ * Caesar.c
+ * A program that encrypts messages using Caesar’s cipher. Your program must
+ * accept a single command-line argument: a non-negative integer. Let’s call it
+ * k for the sake of discussion. If your program is executed without any
+ * command-line arguments or with more than one command-line argument, your
+ * program should yell at the user and return a value of 1.
+ * 
+ * */
+ 
 int main(int argc, string argv[])
 {
 if (argc == 2)
-    {
+{
    int key = atoi(argv[1]);
-   printf("what s your message?");
-   string plaintext = get_string();
-   string ciphertext;
-   
-   for (i=0; i < strlen(plaintext); i++)
+    
+   if ( key < 0 )
    {
-       if (isalpha (plaintext(i)))
+       printf("negative number!"); 
+       return 2;
+   }
+   else 
+   {
+      printf("what s your message?");
+       
+   string pt = get_string();
+
+   for (i=0; i < strlen(pt); i++)
+   {
+       if (isalpha (pt[i]))
        {
-           if (isupper (plaintext(i)))
+           if (isupper (pt[i]))
            {
-               ciphertext = ((plaintext(i) + key)% 26 )+ 65
-               printf("%c", plaintext(i));
+               printf("%c", (((pt[i] + key) - 65) % 26) + 65));
            }
            else 
            {
-               ciphertext = ((plaintext(i) + key)% 26) + 97
-               printf("%c", plaintext(i));
+               printf("%c",((pt[i] + key) - 97 )% 26) + 97));
            }
        }
        else
        {
-           printf("%c", plaintext(i));
+           printf("%c", plaintext[i]);
        }
    }
      }
    else 
    {    
-    printf("Error!! try again!!")
+    printf("error more or less than 2");
         return 1;
+   }
    }
     return 0;
 }
