2820adfad6b5f339f24ebc8de5e3341435f8cae3
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-02-01 05:00:42 +0800

    Update ceasar1.c
    a = 97
    A = 65

--- ceasar1.c
+++ ceasar1.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main(int argc, string argv[])
 {
@@ -10,22 +11,33 @@ if (argc == 2)
    int key = atoi(argv[1]);
    printf("what s your message?");
    string plaintext = get_string();
-    
+   string ciphertext;
+   
    for (i=0; i < strlen(plaintext); i++)
    {
        if (isalpha (plaintext(i)))
        {
-           if (isupper
-           printf
+           if (isupper (plaintext(i)))
+           {
+               ciphertext = ((plaintext(i) + key)% 26 )+ 65
+               printf("%c", plaintext(i));
+           }
+           else 
+           {
+               ciphertext = ((plaintext(i) + key)% 26) + 97
+               printf("%c", plaintext(i));
+           }
        }
        else
        {
-           printf("%d", plaintext(i));
+           printf("%c", plaintext(i));
        }
    }
      }
    else 
    {    
     printf("Error!! try again!!")
+        return 1;
    }
+    return 0;
 }
