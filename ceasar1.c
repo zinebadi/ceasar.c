bfe7f2ec69a48bf1a7864927d1401527a24c9bf7
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-02-01 03:57:37 +0800

    Update ceasar1.c

--- ceasar1.c
+++ ceasar1.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-if (argc != 2)
+if (argc == 2)
     {
-    printf("Error!! try again")
-     }
-   else 
-   {
    int key = atoi(argv[1]);
    printf("what s your message?");
    string plaintext = get_string();
-   
-
-   
-   
-   
+    
+   for (i=0; i < strlen(plaintext); i++)
+   {
+       
    }
-   
-  
+     }
+   else 
+   {    
+    printf("Error!! try again!!")
+   }
 }
