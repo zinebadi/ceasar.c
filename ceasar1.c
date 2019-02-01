3e16d262a7a73d926319bd86fb127c1cf2bec27d
zinebadi <45413450+zinebadi@users.noreply.github.com>
2019-02-02 06:35:52 +0800

    Update ceasar1.c

--- ceasar1.c
+++ ceasar1.c
@@ -3,58 +3,51 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-// **
- * Caesar.c
- * A program that encrypts messages using Caesar’s cipher. Your program must
- * accept a single command-line argument: a non-negative integer. Let’s call it
- * k for the sake of discussion. If your program is executed without any
- * command-line arguments or with more than one command-line argument, your
- * program should yell at the user and return a value of 1.
- * 
- * */
- 
+
+// A program that encrypts messages using Caesar’s cipher. Your program must//
+
 int main(int argc, string argv[])
 {
-if (argc == 2)
-{
-   int key = atoi(argv[1]);
-    
-   if ( key < 0 )
-   {
-       printf("negative number!"); 
-       return 2;
-   }
-   else 
-   {
-      printf("what s your message?");
-       
-   string pt = get_string();
+    if (argc != 2)
+    {
+    printf("error more or less than 2\n");
+    return 1;
+    }
+    else
+    {
 
-   for (i=0; i < strlen(pt); i++)
-   {
-       if (isalpha (pt[i]))
-       {
-           if (isupper (pt[i]))
-           {
-               printf("%c", (((pt[i] + key) - 65) % 26) + 65));
-           }
-           else 
-           {
-               printf("%c",((pt[i] + key) - 97 )% 26) + 97));
-           }
-       }
-       else
-       {
-           printf("%c", pt[i]);
-       }
-     }
-  }
-   else 
-   {    
-    printf("error more or less than 2");
-        return 1;
-   }
-   }
-  printf("\n");
-    return 0;
+       int key = atoi(argv[1]);
+
+        if ( key < 0 )
+        {
+            printf("negative number!\n");
+            return 2;
+         }
+         else
+        {
+            printf("what s your message?\n");
+            string pt = get_string();
+
+        for (int i = 0; i < strlen(pt); i++)
+         {
+            if (isalpha (pt[i]))
+            {
+                if (isupper (pt[i]))
+                {
+                  printf("%c", ((((pt[i] + key) - 65) % 26) + 65));
+                }
+                else
+                {
+                  printf("%c",((((pt[i] + key) - 97 )% 26) + 97));
+                }
+             }
+            else
+            {
+               printf("%c", pt[i]);
+            }
+          }
+        printf("\n");
+        return 0;
+        }
+    }
 }
